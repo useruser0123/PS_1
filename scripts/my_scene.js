5179fe55f448c7b717463de1eef32ff32d825b72
Fujiwara <113399284+useruser0123@users.noreply.github.com>
2023-11-29 14:23:25 +0900

    演習1-3完了

--- scripts/my_scene.js
+++ scripts/my_scene.js
@@ -1,30 +1,47 @@
 
 // MyScene1クラス
 // 他のJSファイルから呼び出された場合はシーンを返す
-class MyScene extends Phaser.Scene {
-
-    // 継承した「Phaser.Scene」クラスのコンストラクタの呼び出し
-    constructor() {
-        super({ key: 'MyScene', active: true });
-    }
+class MyScene extends Phaser.Scene{
 
     // シーンの事前読み込み処理
     preload() {
          // 画像の読み込み(使用する時の名前, パス)
-        this.load.image('sky', 'assets/sky.png');
-        this.load.image('alien1', 'assets/alien1.png');
+        this.load.image('background', 'assets/background.png');
+        this.load.image('taro', 'assets/taro.png');
     }
 
     // シーン初期化処理
     create() {
          // 単体画像をシーンに追加(X座標,Y座標,画像名)
-        this.add.image(D_WIDTH/2, D_HEIGHT/2, 'sky');
-        this.add.image(D_WIDTH/2, D_HEIGHT/2, 'alien1');
-        this.text = this.add.text(10, 10, 'Scene 1').setFontSize(32).setColor('#ff0');
+        this.add.image(D_WIDTH/2, D_HEIGHT/2, 'background');
+        const player = this.physics.add.sprite(500, 350, 'taro');
+        this.player = player
+        this.player.angle = 0;
     }
     
   // 毎フレーム実行される繰り返し処理
-    update() {
-    }
-
+    update(time, delta) {
+        // プレイヤーの向きフラグを変更
+        if (this.player.x >= D_WIDTH - 100) this.player_direction = -1;
+        if (this.player.x <= 450) this.player_direction = 1;
+        // プレイヤーの移動
+        // +X方向の移動フラグならプレイヤーを右に移動
+        if (this.player_direction == 1) {
+            this.player.setVelocityX(100);
+            this.player.setVelocityY(-100);
+        // -X方向の移動フラグならプレイヤーを左に移動
+        } else {
+          this.player.setVelocityX(-100);
+          this.player.setVelocityY(100);
+        }
+       // if (this.player.y >= D_WIDTH - 400) this.player_direction3 = -1;
+       // if (this.player.y <= 30) this.player_direction3 = 1;
+       // if (this.player.x >= D_WIDTH - 100) this.player_direction3 = -1;
+       // if (this.player.x <= 0) this.player_direction3 = 1;
+       // // プレイヤーの移動
+       //     this.player.y -= 5;// 横方向へ移動を設定
+       //     this.player.x += 5;// 横方向へ移動を設定
+       // this.player.angle += 5;
+       // this.player.setAngle( this.player.angle );
+  }
 }
